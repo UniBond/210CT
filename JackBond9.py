b5f14e1b0c96080b76f3dfb98eba400de1c5fa7d
UniBond <bondj5@uni.coventry.ac.uk>
2016-12-01 09:53:47 +0000

    Update JackBond9.py

--- JackBond9.py
+++ JackBond9.py
@@ -1,19 +1,19 @@
-def binarySearch(theList, num):
-    firstNumber = 0 #Start from 0
+def binarySearch(theList, num1, num2):
+    firstNumber = 0
     lastNumber = len(theList) - 1
     numberFound = False
 
     while firstNumber <= lastNumber and not numberFound:
         middleValue = (firstNumber + lastNumber) // 2
-        if theList[middleValue] == num:
+        if theList[middleValue] > num1 and theList[middleValue] < num2:
             numberFound = True
 
         else:
-            if num < theList[middleValue]:
+            if num1 < theList[middleValue]:
                 lastNumber = middleValue - 1
             else:
                 firstNumber = middleValue + 1
     return numberFound
 
 
-print(binarySearch([1,3,5,6,7,3,8,22,19,1], 22))
+print(binarySearch([2,3,5,7,9,13], 10, 13))
