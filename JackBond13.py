44d13ec1a59de672084975b36922a674d8bd3bf9
UniBond <bondj5@uni.coventry.ac.uk>
2016-12-01 11:28:05 +0000

    Update JackBond13.py
    Comments added.

--- JackBond13.py
+++ JackBond13.py
@@ -1,51 +1,63 @@
+#Node class, handles everything to do with nodes themselves
 class Node(object):
+    #initialization
     def __init__(self, value):
         self.value = value
         self.adjacent = {}
 
+    #add adjacent node
     def add_next(self, nextTo):
         self.adjacent[nextTo] = nextTo
 
+    #return nodes value (not node location)
     def get_value(self):
-        return(self.value)
-    
-    def get_adjacents(self):
+        return self.value
+
+    #return length of adjacent dictionary
+    def get_adjacent_length(self):
         return len(self.adjacent)
-    
-    def get_adjacent(self):
+
+    #return adjacent dictionary as a list
+    def get_adjacent_as_list(self):
         printList = []
         for item in self.adjacent:
             printList.append(self.adjacent[item].get_value())
         return printList
 
+    #return adjacent dictionary as a dictionary
     def return_adjacent(self):
         return self.adjacent
-        
+
+#Graph class, allows the actual creation of the graph, and handles all graph related queries
 class Graph:
+    #initialization
     def __init__(self):
         self.dict = {}
 
+    #creates and adds node to graph
     def add_node(self, value):
         node = Node(value)
         self.dict[value] = node
         print("Node: " + str(value) + " has been created")
 
-    def add_edge(self, value, to):
-        self.dict[value].add_next(self.dict[to])
-        self.dict[to].add_next(self.dict[value])
+    #adds adjacent node to node
+    def add_edge(self, node, adjacent_node):
+        self.dict[node].add_next(self.dict[adjacent_node])
+        self.dict[adjacent_node].add_next(self.dict[node])
 
+    #print graph in a readable form
     def print_graph(self):
         keyList = []
         for key in self.dict:
             keyList.append(key)
         for item in keyList:
             print(str(item) + ", adjacent nodes: " +
-                  str(self.dict[item].get_adjacent()) +
-                  " (" + str(self.dict[item].get_adjacents()) + ")")
+                  str(self.dict[item].get_adjacent_as_list()) +
+                  " (" + str(self.dict[item].get_adjacent_length()) + ")")
 
 
 
-
+#Runs program with nodes that allow it to work.
 if __name__ == '__main__':
 
     graph = Graph()
@@ -65,5 +77,6 @@ if __name__ == '__main__':
     graph.add_edge(9, 2)
     graph.add_edge(2, 5)
     graph.print_graph()
+
     
 
