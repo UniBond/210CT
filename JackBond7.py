e5c218abd84f54d1273c869a69207e534e937ef3
UniBond <bondj5@uni.coventry.ac.uk>
2016-12-01 12:39:30 +0000

    Update JackBond7.py
    Comment added.

--- JackBond7.py
+++ JackBond7.py
@@ -1,13 +1,29 @@
+#Gets user input
 newInt = int(input("> "))
 
+#function that works out if a number is a prime number
 def primeNumber(num, n):
-    newNum = num % n
-    if n <= 1:
+    newNum = num % n #variable. num argument, moduler n argument, newNum = remainder
+    #print(str(newNum) + ", " + str(num) + ", " + str(n)) testing
+    if n <= 1: #if n < 1, then its prime!
         print(str(num) + " is a prime number!")
         return
-    if newNum == 0:
+    if newNum == 0: #if remainder is 0, its been divided perfectly, no remainder
         print(str(num) + " is not a prime number!")
         return
-    primeNumber(num, n-1)
+    #recursive call, keeps reducing to see if prime
+    primeNumber(num, n-1) 
 
+
+#function call. allows for testing, with arguments from users input
 primeNumber(newInt, newInt-1)
+        
+
+
+#PRIMENUMBER(NUM, N)
+#   INT = NUM modulo n
+#   if n <<- 1
+#       RETURN TRUE
+#   if INT <-<- 0
+#       RETURN FALSE
+#   primeNumber(num, n-1)
