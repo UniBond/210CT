0497f99bbbebf229cb42c3bddee30d37bcfa47fa
UniBond <bondj5@uni.coventry.ac.uk>
2016-12-01 12:47:48 +0000

    Update JackBond6.py
    Comments added.

--- JackBond6.py
+++ JackBond6.py
@@ -1,9 +1,16 @@
+#function definition. takes 1 argument
 def flipWord(sentence):
+    #variable declaration
     wordList = []
+    #split sentence into words, add each word as list item
     wordList = sentence.split()
     newString = ""
+    #each word in list is flipped backwards, loop over each word
     for word in wordList[::-1]:
+        #add word to string, flipped alongside space.
         newString = newString + word + " "
-    return newString
+    return newString #return string
 
+#test function and print
 print(flipWord("This is awesome"))
+
