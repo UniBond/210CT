c304d72063984c7d45ebc6978c5b513006529846
UniBond <bondj5@uni.coventry.ac.uk>
2016-12-01 12:48:58 +0000

    Update JackBond5.py
    Comments added.

--- JackBond5.py
+++ JackBond5.py
@@ -1,34 +1,36 @@
 class matrix:
 
-    def addition(m1, m2):
-        newMatrix = [[0,0], [0,0]]
-        for i in range(len(m1)):
-            for j in range(len(m2)):
-                newMatrix[i][j] = m1[i][j] + m2[i][j]
+    def addition(m1, m2):  #function for adding matrices
+        newMatrix = [[0,0], [0,0]]  #define 2 lists within list
+        for i in range(len(m1)): #loop length of argument m1
+            for j in range(len(m2)): #loop length of argument m2
+                newMatrix[i][j] = m1[i][j] + m2[i][j] #append added 
+                                                       #matricies to list                                                                                         
 
-        return newMatrix
+        return newMatrix #return
 
-    def subtraction(m1, m2):
-        newMatrix = [[0,0], [0,0]]
-        for i in range(len(m1)):
-            for j in range(len(m2)):
-                newMatrix[i][j] = m1[i][j] - m2[i][j]
+    def subtraction(m1, m2): #function for subtracting matrices
+        newMatrix = [[0,0], [0,0]] #define 2 lists within list
+        for i in range(len(m1)): #loop length of argument m1
+            for j in range(len(m2)): #loop length of argument m2
+                newMatrix[i][j] = m1[i][j] - m2[i][j] #append added
+                                                       #matricies to list
 
-        return newMatrix
+        return newMatrix #return
 
 
-    def multiply(m1, m2):
-        newMatrix = [[0,0], [0,0]]
-        try:
-            for i in range(2):
-                for j in range(2):
+    def multiply(m1, m2): #function for multiplying matrices
+        newMatrix = [[0,0], [0,0]] #define 2 lists within list
+        try:                       #try (allows for exceptions)
+            for i in range(2): #loop twice
+                for j in range(2): #loop twice
                     newMatrix[i][j] = m1[i][j] * m2[i][j]
-        except TypeError:
-            for i in range(2):
-                for j in range(2):
+        except TypeError: #except TypeError
+            for i in range(2): #loop twice
+                for j in range(2): #loop twice
                     newMatrix[i][j] = m1[i] * m2[j]
 
-        return newMatrix
+        return newMatrix #return
 
 b = [[1,2], [3,4]]
 c = [[2,3], [4,5]] 
